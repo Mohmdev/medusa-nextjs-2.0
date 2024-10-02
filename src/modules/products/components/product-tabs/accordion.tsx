c3ebfda05516a925b89cef47dcadaa22dc7a3402
Mohamad <Mohammadr_m@outlook.com>
2024-10-02 13:22:14 +0000

    Fix: Remove unused '@ts-expect-error' directives. Remove bugged query `product.tags` from related products query.

--- src/modules/products/components/product-tabs/accordion.tsx
+++ src/modules/products/components/product-tabs/accordion.tsx
@@ -1,6 +1,6 @@
-import { Text, clx } from "@medusajs/ui"
-import * as AccordionPrimitive from "@radix-ui/react-accordion"
-import React from "react"
+import { Text, clx } from '@medusajs/ui'
+import * as AccordionPrimitive from '@radix-ui/react-accordion'
+import React from 'react'
 
 type AccordionItemProps = AccordionPrimitive.AccordionItemProps & {
   title: string
@@ -9,7 +9,7 @@ type AccordionItemProps = AccordionPrimitive.AccordionItemProps & {
   required?: boolean
   tooltip?: string
   forceMountContent?: true
-  headingSize?: "small" | "medium" | "large"
+  headingSize?: 'small' | 'medium' | 'large'
   customTrigger?: React.ReactNode
   complete?: boolean
   active?: boolean
@@ -18,18 +18,13 @@ type AccordionItemProps = AccordionPrimitive.AccordionItemProps & {
 }
 
 type AccordionProps =
-  | (AccordionPrimitive.AccordionSingleProps &
-      React.RefAttributes<HTMLDivElement>)
-  | (AccordionPrimitive.AccordionMultipleProps &
-      React.RefAttributes<HTMLDivElement>)
+  | (AccordionPrimitive.AccordionSingleProps & React.RefAttributes<HTMLDivElement>)
+  | (AccordionPrimitive.AccordionMultipleProps & React.RefAttributes<HTMLDivElement>)
 
 const Accordion: React.FC<AccordionProps> & {
   Item: React.FC<AccordionItemProps>
 } = ({ children, ...props }) => {
-  return (
-    /* @ts-expect-error */
-    <AccordionPrimitive.Root {...props}>{children}</AccordionPrimitive.Root>
-  )
+  return <AccordionPrimitive.Root {...props}>{children}</AccordionPrimitive.Root>
 }
 
 const Item: React.FC<AccordionItemProps> = ({
@@ -38,30 +33,23 @@ const Item: React.FC<AccordionItemProps> = ({
   description,
   children,
   className,
-  headingSize = "large",
+  headingSize = 'large',
   customTrigger = undefined,
   forceMountContent = undefined,
   triggerable,
   ...props
 }) => {
   return (
-    /* @ts-expect-error */
     <AccordionPrimitive.Item
       {...props}
-      className={clx(
-        "border-grey-20 group border-t last:mb-0 last:border-b",
-        "py-3",
-        className
-      )}
+      className={clx('group border-t border-grey-20 last:mb-0 last:border-b', 'py-3', className)}
     >
-      {/* @ts-expect-error */}
       <AccordionPrimitive.Header className="px-1">
         <div className="flex flex-col">
           <div className="flex w-full items-center justify-between">
             <div className="flex items-center gap-4">
-              <Text className="text-ui-fg-subtle text-sm">{title}</Text>
+              <Text className="text-sm text-ui-fg-subtle">{title}</Text>
             </div>
-            {/* @ts-expect-error */}
             <AccordionPrimitive.Trigger>
               {customTrigger || <MorphingTrigger />}
             </AccordionPrimitive.Trigger>
@@ -73,11 +61,10 @@ const Item: React.FC<AccordionItemProps> = ({
           )}
         </div>
       </AccordionPrimitive.Header>
-      {/* @ts-expect-error */}
       <AccordionPrimitive.Content
         forceMount={forceMountContent}
         className={clx(
-          "radix-state-closed:animate-accordion-close radix-state-open:animate-accordion-open radix-state-closed:pointer-events-none px-1"
+          'radix-state-closed:animate-accordion-close radix-state-open:animate-accordion-open radix-state-closed:pointer-events-none px-1'
         )}
       >
         <div className="inter-base-regular group-radix-state-closed:animate-accordion-close">
@@ -93,10 +80,10 @@ Accordion.Item = Item
 
 const MorphingTrigger = () => {
   return (
-    <div className="text-grey-90 hover:bg-grey-5 active:bg-grey-5 active:text-violet-60 focus:border-violet-60 disabled:text-grey-30 bg-transparent disabled:bg-transparent rounded-rounded group relative p-[6px]">
+    <div className="active:text-violet-60 focus:border-violet-60 group relative rounded-rounded bg-transparent p-[6px] text-grey-90 hover:bg-grey-5 active:bg-grey-5 disabled:bg-transparent disabled:text-grey-30">
       <div className="h-5 w-5">
-        <span className="bg-grey-50 rounded-circle group-radix-state-open:rotate-90 absolute inset-y-[31.75%] left-[48%] right-1/2 w-[1.5px] duration-300" />
-        <span className="bg-grey-50 rounded-circle group-radix-state-open:rotate-90 group-radix-state-open:left-1/2 group-radix-state-open:right-1/2 absolute inset-x-[31.75%] top-[48%] bottom-1/2 h-[1.5px] duration-300" />
+        <span className="group-radix-state-open:rotate-90 absolute inset-y-[31.75%] left-[48%] right-1/2 w-[1.5px] rounded-circle bg-grey-50 duration-300" />
+        <span className="group-radix-state-open:rotate-90 group-radix-state-open:left-1/2 group-radix-state-open:right-1/2 absolute inset-x-[31.75%] bottom-1/2 top-[48%] h-[1.5px] rounded-circle bg-grey-50 duration-300" />
       </div>
     </div>
   )
--- src/modules/products/components/related-products/index.tsx
+++ src/modules/products/components/related-products/index.tsx
@@ -1,17 +1,14 @@
-import Product from "../product-preview"
-import { getRegion } from "@/lib/data/regions"
-import { getProductsList } from "@/lib/data/products"
-import { HttpTypes } from "@medusajs/types"
+import Product from '../product-preview'
+import { getRegion } from '@/lib/data/regions'
+import { getProductsList } from '@/lib/data/products'
+import { HttpTypes } from '@medusajs/types'
 
 type RelatedProductsProps = {
   product: HttpTypes.StoreProduct
   countryCode: string
 }
 
-export default async function RelatedProducts({
-  product,
-  countryCode,
-}: RelatedProductsProps) {
+export default async function RelatedProducts({ product, countryCode }: RelatedProductsProps) {
   const region = await getRegion(countryCode)
 
   if (!region) {
@@ -20,26 +17,27 @@ export default async function RelatedProducts({
 
   // edit this function to define your related products logic
   const queryParams: HttpTypes.StoreProductParams = {}
+
   if (region?.id) {
     queryParams.region_id = region.id
   }
+
   if (product.collection_id) {
     queryParams.collection_id = [product.collection_id]
   }
-  if (product.tags) {
-    queryParams.tags = product.tags
-      .map((t) => t.value)
-      .filter(Boolean) as string[]
-  }
+
+  // TODO: Fix this query
+  // This query is bugged: Tags are not recognized.
+  // if (product.tags) {
+  //   queryParams.tags = product.tags.map((t) => t.value).filter(Boolean) as string[]
+  // }
   queryParams.is_giftcard = false
 
   const products = await getProductsList({
     queryParams,
     countryCode,
   }).then(({ response }) => {
-    return response.products.filter(
-      (responseProduct) => responseProduct.id !== product.id
-    )
+    return response.products.filter((responseProduct) => responseProduct.id !== product.id)
   })
 
   if (!products.length) {
@@ -48,16 +46,14 @@ export default async function RelatedProducts({
 
   return (
     <div className="product-page-constraint">
-      <div className="flex flex-col items-center text-center mb-16">
-        <span className="text-base-regular text-gray-600 mb-6">
-          Related products
-        </span>
-        <p className="text-2xl-regular text-ui-fg-base max-w-lg">
+      <div className="mb-16 flex flex-col items-center text-center">
+        <span className="text-base-regular mb-6 text-gray-600">Related products</span>
+        <p className="text-2xl-regular max-w-lg text-ui-fg-base">
           You might also want to check out these products.
         </p>
       </div>
 
-      <ul className="grid grid-cols-2 small:grid-cols-3 medium:grid-cols-4 gap-x-6 gap-y-8">
+      <ul className="grid grid-cols-2 gap-x-6 gap-y-8 small:grid-cols-3 medium:grid-cols-4">
         {products.map((product) => (
           <li key={product.id}>
             <Product region={region} product={product} />
