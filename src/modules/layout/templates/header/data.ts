8922e493a1c2c3b1abb071cf42d1ac022ba256eb
Mohmdev <Mohammadr_m@outlook.com>
2024-11-07 15:12:15 +0300

    blank

--- src/modules/layout/templates/header/data.ts
+++
@@ -1,8 +0,0 @@
-import { enrichLineItems, retrieveCart } from '@/lib/data/cart'
-
-const cart = await retrieveCart()
-if (cart?.items?.length) {
-  cart.items = await enrichLineItems(cart.items, cart.region_id!)
-}
-
-export { cart as cartItems }
