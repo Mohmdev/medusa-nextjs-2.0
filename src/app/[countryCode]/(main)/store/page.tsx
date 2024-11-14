78af82ec8b81dfefebed5f3de4f69ab837b11e83
Mohamad <Mohammadr_m@outlook.com>
2024-11-14 11:12:15 +0000

    init

--- src/app/[countryCode]/(main)/store/page.tsx
+++ src/app/[countryCode]/(main)/store/page.tsx
@@ -1,6 +1,9 @@
+import { Suspense } from "react"
 import { Metadata } from "next"
+import SkeletonProductGrid from "@/modules/skeletons/templates/skeleton-product-grid"
+import PaginatedProducts from "@/modules/store/components/paginated-products"
+import RefinementList from "@/modules/store/components/refinement-list"
 import { SortOptions } from "@/modules/store/components/refinement-list/sort-products"
-import StoreTemplate from "@/modules/store/templates/store-1"
 
 export const metadata: Metadata = {
   title: "Store",
@@ -22,11 +25,27 @@ export default async function StorePage(props: Params) {
   const searchParams = await props.searchParams
   const { sortBy, page } = searchParams
 
+  const pageNumber = page ? parseInt(page) : 1
+  const sort = sortBy || "created_at"
+
   return (
-    <StoreTemplate
-      sortBy={sortBy}
-      page={page}
-      countryCode={params.countryCode}
-    />
+    <div
+      className="flex flex-col small:flex-row small:items-start py-6 content-container"
+      data-testid="category-container"
+    >
+      <RefinementList sortBy={sort} />
+      <div className="w-full">
+        <div className="mb-8 text-2xl-semi">
+          <h1 data-testid="store-page-title">All products</h1>
+        </div>
+        <Suspense fallback={<SkeletonProductGrid />}>
+          <PaginatedProducts
+            sortBy={sort}
+            page={pageNumber}
+            countryCode={params.countryCode}
+          />
+        </Suspense>
+      </div>
+    </div>
   )
 }
